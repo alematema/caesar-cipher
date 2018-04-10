1784c2132a758277fc093ee0a2ece2481b56fbeb
alematema <alematema@gmail.com>
2018-04-10 11:11:08 -0300

    script para atualizar historico criptografias

---
+++ js/historico_criptografias.js
@@ -0,0 +1,22 @@
+function atualizarHistoricoCriptografias(){
+	
+	if (document.getElementById('lista-criptografias').children.length > 0){
+			
+			document.querySelector('#lista-criptografias').classList.remove('invisivel');
+			document.querySelector('.sem-historico-criptografias').classList.add('invisivel');
+			document.querySelector('.sem-historico-msgm').classList.add('invisivel');
+			
+			document.querySelector('.pesquisar-historico-input').disabled = false;
+		
+			
+		}else{
+			
+			document.querySelector('#lista-criptografias').classList.add('invisivel');
+			document.querySelector('.sem-historico-criptografias').classList.remove('invisivel');
+			document.querySelector('.sem-historico-msgm').classList.remove('invisivel');
+			
+			document.querySelector('.pesquisar-historico-input').disabled = true;
+			
+		}
+	
+}
