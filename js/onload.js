c42dcf1d81c989868afb7a67765cbd9dc9e7cee3
alematema <alematema@gmail.com>
2018-04-10 11:20:06 -0300

    scripts para modal e onload

---
+++ js/onload.js
@@ -0,0 +1,9 @@
+	function heightMe() {
+		document.getElementById('content').style.height = window.innerHeight * 0.90 + 'px';
+	}
+
+
+	function setUp(event) {
+		console.log('setting up ...');
+		atualizarHistoricoCriptografias();
+	}
