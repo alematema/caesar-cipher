02587c25a19e8c41373e053776941c87863798fc
alematema <alematema@gmail.com>
2018-04-10 22:50:49 -0300

    output da criptografia implementado

--- js/cipher.js
+++ js/cipher.js
@@ -1,7 +1,9 @@
+var header = document.querySelector('#header');
 var btnCriptografar = document.querySelector('#btn-criptografar');
 var input = document.querySelector('#input');
 var output = document.querySelector('#output');
 var fraseCriptografadaContainer = document.querySelector('#frase-criptografada-container');
+var fraseDoInputContainer = document.querySelector('#frase-do-input-container');
 var myInterval;
 var criptoArrayGB, srcArrayGB;
 var init = new Date().getTime();
@@ -9,27 +11,34 @@ var i = 0; //controla duration de  duracaoCriptoUmaLetra ms
 var j = 0; // indice para navegar 1 letra do array a cada duracaoCriptoUmaLetra ms
 var duracaoCriptoUmaLetra = 2000;
 
-
+header.clear = clear;
+header.restore = restore;
+header.writeOut = writeOut;
 btnCriptografar.criptografar = criptografar;
 input.atualizarBotaoCriptografar = atualizarBotaoCriptografar;
 output.write = write;
+output.writeSrc = writeSrc;
 output.fraseCriptografadaContainer = fraseCriptografadaContainer;
+output.fraseDoInputContainer = fraseDoInputContainer;
 output.addLetraCriptografada = addLetraCriptografada;
+output.selecionarLetra = selecionarLetra;
+output.deselecionarLetra = deselecionarLetra;
 fraseCriptografadaContainer.toCharArray = toCharArray;
+fraseDoInputContainer.getLetra = getLetra;
 
-function toCharArray(){
-	
-	var charArray=[];
-	
-	for(var i = 0; i < this.children.length; i++){
-		
-		
+function toCharArray() {
+
+	var charArray = [];
+
+	for (var i = 0; i < this.children.length; i++) {
+
+
 		charArray.push(this.children[i].textContent);
-		
+
 	}
-	
+
 	return charArray;
-	
+
 }
 
 
@@ -47,7 +56,7 @@ function addLetraCriptografada(letra) {
 
 	fraseArray.forEach(char => {
 
-		span = getSpan(char);
+		span = newSpan(char, 'letra');
 		fraseCriptografadaContainer.appendChild(span);
 
 	});
@@ -57,20 +66,68 @@ function addLetraCriptografada(letra) {
 
 }
 
-function getSpan(char) {
+function getLetra(indexLetra) {
+	return this.children[indexLetra];
+}
+
+function newSpan(char, cssClass) {
 
 	var span = document.createElement('span');
-	span.classList.add('letra');
+	span.classList.add(cssClass);
 	span.textContent = char;
 
 	return span;
 
 }
 
+function clear(){
+	
+	document.getElementById('some-text').textContent='';
+	input.classList.add('invisivel');
+	document.querySelector('.lock-key').textContent = '';
+	document.querySelector('.key-input').classList.add('invisivel');
+	btnCriptografar.classList.add('invisivel');
+		
+}
+
+function restore(){
+	
+	document.getElementById('some-text').textContent='Caesar Cipher';
+	input.classList.remove('invisivel');
+	input.value='';
+	document.querySelector('.lock-key').innerHTML = '&#128272;';
+	document.querySelector('.key-input').classList.remove('invisivel');
+	btnCriptografar.classList.remove('invisivel');
+	input.atualizarBotaoCriptografar();
+	
+}
+
+function writeOut(string){
+	
+	document.getElementById('some-text').textContent=string;
+	
+}
+
+function writeSrc(srcArray) {
+
+
+	srcArray.forEach(char => {
+
+		fraseDoInputContainer.appendChild(newSpan(char, 'letra-input'));
+
+	});
+
+	output.selecionarLetra(0);
+
+}
+
 function write(criptoArray, srcArray) {
 
-	console.log(criptoArray.join(''));
-	console.log(srcArray.join(''));
+	output.writeSrc(srcArray);
+	
+	header.clear();
+	header.writeOut('Criptografando...');
+
 	document.querySelector('.cipher-engine-gif').classList.remove('invisivel');
 	document.querySelector('#letra-em-criptografia').classList.remove('invisivel');
 	document.querySelector('#letra-em-criptografia').textContent = srcArray[0];
@@ -81,11 +138,20 @@ function write(criptoArray, srcArray) {
 
 	init = new Date().getTime();
 
-
-
 	myInterval = setInterval(myFn, 10);
 
+}
 
+function selecionarLetra(indexLetra) {
+
+	var span = this.fraseDoInputContainer.getLetra(indexLetra);
+	if (span) span.classList.add('selecionada-input');
+
+}
+
+function deselecionarLetra(indexLetra) {
+	var span = this.fraseDoInputContainer.getLetra(indexLetra);
+	if (span) span.classList.remove('selecionada-input');
 }
 
 
@@ -112,12 +178,11 @@ function myFn() {
 	i += 10;
 
 	if (i >= duracaoCriptoUmaLetra) {
-
-
 		output.addLetraCriptografada(criptoArrayGB[j]);
-
 		i = 0;
+		output.deselecionarLetra(j);
 		j++;
+		output.selecionarLetra(j);
 		if (j >= srcArrayGB.length) j = 0;
 	}
 
@@ -129,12 +194,13 @@ function myFn() {
 		document.querySelector('#letra-em-criptografia').classList.add('invisivel');
 		i = 0; //controla duration de duracaoCriptoUmaLetra ms 
 		j = 0; // indice para navegar 1 letra do array a cada duracaoCriptoUmaLetra ms
+		header.restore();
 	}
 
 }
 
 function atualizarBotaoCriptografar(event) {
-	if (event.srcElement.value.trim() == '') {
+	if (input.value.trim() == '') {
 		btnCriptografar.disabled = true;
 	} else {
 		btnCriptografar.disabled = false;
